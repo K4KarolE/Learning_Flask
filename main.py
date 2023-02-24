b11b2f9e1a98bef2791cb18dc5fc66f10db44afc
karoly.egyed@protonmail.com <karoly.egyed@protonmail.com>
2023-02-24 15:34:06 +0000

    Template Inheritance

--- main.py
+++ main.py
@@ -19,20 +19,3 @@ def market_page():
 
     ]
     return render_template('market.html', items=items)
-
-
-
-
-
-# @app.route("/")
-# def hello_world():
-#     return "<h1>Hello, World!</h1> \n <a href='http://127.0.0.1:5000/about'>About</a>"
-
-
-# @app.route('/about/<username>')
-# def about_page(username):
-#     return f'<h1>About page of {username}</h1>'
-
-# @app.route('/about')
-# def about_page():
-#     return '<h1>Learning Flask</h1>'
